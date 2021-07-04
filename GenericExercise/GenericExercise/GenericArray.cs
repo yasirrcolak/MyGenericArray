c0859d9f220b033a31648f26adbd8d9e4f716f9d
Muhammet Yasir Çolak <76009454+yasirrcolak@users.noreply.github.com>
2021-07-04 21:41:43 +0300

    First commit

---
+++ GenericExercise/GenericExercise/GenericArray.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace GenericExercise
+{
+    public class GenericArray<T> where T : struct
+    {
+        /// <summary>
+        /// T type array containing items.
+        /// </summary>
+        private T[] array = new T[2];
+
+        /// <summary>
+        /// The number of how many items are in the T-type array containing the items.
+        /// </summary>
+        private int _itemCount = 0;
+
+        /// <summary>
+        /// The number of how many items are in the T-type array containing the items.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return _itemCount;
+            }
+        }
+
+        #region Public Methods  
+
+        /// <summary>
+        /// Array is print to screen.
+        /// </summary>
+        public void PrintArray()
+        {
+            if (_itemCount == 0)
+            {
+                Console.WriteLine("dizi boş\n");
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine("dizi : " + array[i] + " ");
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+
+        /// <summary>
+        /// This method add item to array. If array is full, call by 'Expand array' method.
+        /// </summary>
+        /// <param name="input">The item to be appended to the end of aray</param>
+        public void Add(T input)
+        {
+            // Expand array if array is full.
+            if (_itemCount >= array.Length)
+            {
+                // Temporary T type array lenght is array lenght * 2.
+                T[] tempArray = new T[array.Length * 2];
+
+                // All items copy to tempArray. 
+                for (int i = 0; i < _itemCount; i++)
+                {
+                    tempArray[i] = array[i];
+                }
+
+                array = tempArray;
+            }
+
+            array[_itemCount] = input;
+
+            _itemCount++;
+        }
+
+        /// <summary>
+        /// This method remove first item to array. If array drops below quarter capacity, call by 'Collapse array' method. 
+        /// </summary>
+        /// <param name="input">The item to be removed from array</param>
+        /// <returns>if removed is succesfuly return true. if removed is unsuccesfully return false.</returns>
+        public bool Remove(T input)
+        {
+            // Collapse array if array drops below quarter capacity.
+            if (_itemCount <= array.Length / 4)
+            {
+                // Temporary T type array lenght is array lenght * 2.
+                T[] temporaryArray = new T[array.Length / 2];
+
+                // All items copy to tempArray.
+                for (int i = 0; i < _itemCount; i++)
+                {
+                    temporaryArray[i] = array[i];
+                }
+
+                array = temporaryArray;
+            }
+
+            // It keeps the information whether the item to be deleted is in the array.
+            bool findCheck = false;
+
+            // Temporary T type array lenght is array lenght.
+            T[] tempArray = new T[array.Length];
+
+            // Contains index value of temporary array in loop.
+            int j = 0;
+
+            for (int i = 0; i < _itemCount; i++)
+            {
+                // if input equals array[i] continue. else array copy to temparray.
+                if (array[i].Equals(input) && findCheck == false)
+                {
+                    findCheck = true;
+                    continue;
+                }
+                else
+                {
+                    tempArray[j] = array[i];
+                    j++;
+                }
+            }
+
+            array = tempArray;
+
+            _itemCount--;
+
+            if (findCheck == true)
+                return true;
+            else
+                return false;
+
+        }
+
+        /// <summary>
+        /// The clear method is used at the remove all items from the array.
+        /// </summary>
+        public void Clear()
+        {
+            T[] tempArray = new T[2];
+
+            array = tempArray;
+
+            _itemCount = 0;
+        }
+
+        /// <summary>
+        /// This method is checks if the entered item is in the array.
+        /// </summary>
+        /// <param name="input">The item to be find from array.</param>
+        /// <returns>if found in array return true. if not found in array return false.</returns>
+        public bool Contains(T input)
+        {
+            for (int i = 0; i < _itemCount; i++)
+            {
+                if (array[i].Equals(input))
+                {
+                    return true;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return false;
+        }
+
+        #endregion
+
+    }
+}
---
+++ GenericExercise/GenericExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GenericExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new array of class "GenericArray".
+            var arr1 = new GenericArray<int>();
+
+            arr1.PrintArray();
+
+            arr1.Add(10);
+
+            arr1.PrintArray();
+
+            arr1.Add(20);
+            arr1.Add(30);
+            arr1.Add(40);
+            arr1.Add(50);
+
+            arr1.PrintArray();
+
+            if (arr1.Remove(40))
+                arr1.PrintArray();
+
+            if (arr1.Contains(99))
+                Console.WriteLine("bulundu.\n");
+            else
+                Console.WriteLine("bulunamadı.\n");
+
+
+            Console.WriteLine(arr1.Count.ToString());
+
+            arr1.Clear();
+
+            arr1.PrintArray();
+
+            Console.ReadKey();
+        }
+    }
+}
